a8a84f931227295aa9f9e9e16650b0506c60e245
Zilla998 <coldssielenssams@gmail.com>
2024-08-29 12:53:08 +0300

    add bot

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import sys
+import random
 
-print('Hello world!')
-
-
-def main():
-    a = input("Enter: ")
-
-    if a == "1":
-        sys.exit()
-
-    else:
-        print("Nice!")
-
-main()
-
-
-def answer():
-    ans = input("Do you want?: ")
-
-    print(ans)
-
-
-answer()
+bot = random.randint(1, 3)
