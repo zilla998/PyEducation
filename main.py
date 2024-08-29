4541eaaf05b8562716f8161a4459f3a9825da4ce
Zilla998 <coldssielenssams@gmail.com>
2024-08-29 12:57:27 +0300

    add player and game

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import random
 
 bot = random.randint(1, 3)
+player = input("Введи 1 - Камень, 2 - Бумага, 3 - Ножницы: ")
+
+if bot == 1 and player == 1:
+    result = "Ничья"
+
+if bot == 1 and player == 2:
+    result = "Ты выйграл"
+
+if bot == 1 and player == 3:
+    result = "Ты проиграл"
+
+if bot == 2 and player == 1:
+    result = "Ты проиграл"
+
+if bot == 2 and player == 2:
+    result = "Ничья"
+
+if bot == 2 and player == 3:
+    result = "Ты выйграл"
+
+if bot == 3 and player == 1:
+    result = "Ты выйграл"
+
+if bot == 3 and player == 2:
+    result = "Ты проиграл"
+
+if bot == 3 and player == 3:
+    result = "Ничья"
